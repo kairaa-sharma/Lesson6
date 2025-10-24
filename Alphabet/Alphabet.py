f0d5677a024d5f4540f41030fee38b86832afc54
Kairaa2014 <kairaasharma22@gmail.com>
2025-10-24 19:01:28 +0530

    demo

---
+++ Alphabet/Alphabet.py
@@ -0,0 +1,7 @@
+# This is a code to check wether an input is a letter in the alphabet or not
+var = str(input("Enter any random letter, number, or arithmethic:"))
+
+if var in "abcdefghijklmnopqrstuvwxyz":
+  print(var, "is in the alphabet!")
+else:
+  print(var, "is not in the alphabet..")
